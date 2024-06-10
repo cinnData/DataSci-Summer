f18d49f6b74fdee89522b9eef8a29777467e4f26
cinnData <canela.miguel@icloud.com>
2024-06-10 11:17:39 +0200

    Update roxbury.py

--- Code/roxbury.py
+++ Code/roxbury.py
@@ -27,8 +27,8 @@ df[['value', 'lot_sqft', 'gross_area', 'living_area']].corr()
 df[['value', 'lot_sqft', 'gross_area', 'living_area']].corr().round(2)
 
 # Q3. Trimming the data #
+df1 = df[df['living_area'].between(1000,2000)]
 df1.plot.scatter(x='living_area', y='value', figsize=(5,5), 
     title='Figure 3. Home value vs living area (trimmed data)', color='gray', s=2,
     xlabel='Living area (sq ft)', ylabel='Home value (thousand USD)');
-df1 = df[df['living_area'].between(1000,2000)]
 df1[['value', 'lot_sqft', 'gross_area', 'living_area']].corr().round(2)
